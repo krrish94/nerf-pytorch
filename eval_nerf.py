b7aa3d3ec7f119736dfbc7fb12227870bd6f5e3d
Krishna Murthy <jatavalk@mila03.server.mila.quebec>
2020-04-14 19:16:00 -0400

    Add eval script
    Signed-off-by: Krishna Murthy <jatavalk@mila03.server.mila.quebec>

---
+++ eval_nerf.py
@@ -0,0 +1,169 @@
+import argparse
+import os
+import time
+
+import imageio
+import numpy as np
+import torch
+import torchvision
+import yaml
+from tqdm import tqdm, trange
+
+import models
+from cfgnode import CfgNode
+from load_blender import load_blender_data
+from load_llff import load_llff_data
+from nerf_helpers import (get_ray_bundle, img2mse, meshgrid_xy, mse2psnr,
+                          positional_encoding)
+from train_utils import eval_nerf
+
+
+def cast_to_image(tensor):
+    # Input tensor is (H, W, 3). Convert to (3, H, W).
+    tensor = tensor.permute(2, 0, 1)
+    # Convert to PIL Image and then np.array (output shape: (H, W, 3))
+    img = np.array(torchvision.transforms.ToPILImage()(tensor.detach().cpu()))
+    return img
+    # # Map back to shape (3, H, W), as tensorboard needs channels first.
+    # return np.moveaxis(img, [-1], [0])
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path to (.yml) config file."
+    )
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        required=True,
+        help="Checkpoint / pre-trained model to evaluate."
+    )
+    parser.add_argument(
+        "--savedir",
+        type=str,
+        help="Save images to this directory, if specified."
+    )
+    configargs = parser.parse_args()
+
+    # Read config file.
+    cfg = None
+    with open(configargs.config, "r") as f:
+        cfg_dict = yaml.load(f, Loader=yaml.FullLoader)
+        cfg = CfgNode(cfg_dict)
+
+    images, poses, render_poses, hwf = None, None, None, None
+    i_train, i_val, i_test = None, None, None
+    if cfg.dataset.type.lower() == "blender":
+        # Load blender dataset
+        images, poses, render_poses, hwf, i_split = load_blender_data(
+            cfg.dataset.basedir,
+            half_res=cfg.dataset.half_res,
+            testskip=cfg.dataset.testskip,
+        )
+        i_train, i_val, i_test = i_split
+        H, W, focal = hwf
+        H, W = int(H), int(W)
+    elif cfg.dataset.type.lower() == "llff":
+        # Load LLFF dataset
+        images, poses, bds, render_poses, i_test = load_llff_data(
+            cfg.dataset.basedir,
+            factor=cfg.dataset.downsample_factor,
+        )
+        hwf = poses[0, :3, -1]
+        H, W, focal = hwf
+        hwf = [int(H), int(W), focal]
+        render_poses = torch.from_numpy(render_poses)
+
+    def encode_position_fn(x):
+        return positional_encoding(
+            x,
+            num_encoding_functions=cfg.models.coarse.num_encoding_fn_xyz,
+            include_input=cfg.models.coarse.include_input_xyz,
+        )
+
+    def encode_direction_fn(x):
+        return positional_encoding(
+            x,
+            num_encoding_functions=cfg.models.coarse.num_encoding_fn_dir,
+            include_input=cfg.models.coarse.include_input_dir,
+        )
+
+    # Device on which to run.
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+
+    # Initialize a coarse resolution model.
+    model_coarse = getattr(models, cfg.models.coarse.type)(
+        num_encoding_fn_xyz=cfg.models.coarse.num_encoding_fn_xyz,
+        num_encoding_fn_dir=cfg.models.coarse.num_encoding_fn_dir,
+        include_input_xyz=cfg.models.coarse.include_input_xyz,
+        include_input_dir=cfg.models.coarse.include_input_dir,
+        use_viewdirs=cfg.models.coarse.use_viewdirs,
+    )
+    model_coarse.to(device)
+
+    # If a fine-resolution model is specified, initialize it.
+    model_fine = None
+    if hasattr(cfg.models, "fine"):
+        model_fine = getattr(models, cfg.models.fine.type)(
+            num_encoding_fn_xyz=cfg.models.fine.num_encoding_fn_xyz,
+            num_encoding_fn_dir=cfg.models.fine.num_encoding_fn_dir,
+            include_input_xyz=cfg.models.fine.include_input_xyz,
+            include_input_dir=cfg.models.fine.include_input_dir,
+            use_viewdirs=cfg.models.fine.use_viewdirs,
+        )
+        model_fine.to(device)
+
+    checkpoint = torch.load(configargs.checkpoint)
+    model_coarse.load_state_dict(checkpoint["model_coarse_state_dict"])
+    if checkpoint["model_fine_state_dict"]:
+        try:
+            model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
+        except:
+            print("The checkpoint has a fine-level model, but it could "
+                  "not be loaded (possibly due to a mismatched config file.")
+
+    model_coarse.eval()
+    if model_fine:
+        model_fine.eval()
+    
+    render_poses = render_poses.float().to(device)
+
+    # Create directory to save images to.
+    os.makedirs(configargs.savedir, exist_ok=True)
+
+    # Evaluation loop
+    times_per_image = []
+    for i, pose in enumerate(tqdm(render_poses)):
+        start = time.time()
+        rgb_coarse = None
+        with torch.no_grad():
+            ray_origins, ray_directions = get_ray_bundle(
+                hwf[0], hwf[1], hwf[2], pose
+            )
+            rgb_coarse, _, _, rgb_fine, _, _ = eval_nerf(
+                hwf[0],
+                hwf[1],
+                hwf[2],
+                model_coarse,
+                model_fine,
+                ray_origins,
+                ray_directions,
+                cfg,
+                mode="validation",
+                encode_position_fn=encode_position_fn,
+                encode_direction_fn=encode_direction_fn,
+            )
+        times_per_image.append(time.time() - start)
+        if configargs.savedir:
+            savefile = os.path.join(configargs.savedir, f"{i:04d}.png")
+            imageio.imwrite(savefile, cast_to_image(rgb_coarse[..., :3]))
+        tqdm.write(f"Avg time per image: {sum(times_per_image) / (i + 1)}")
+
+
+if __name__ == "__main__":
+    main()
+
